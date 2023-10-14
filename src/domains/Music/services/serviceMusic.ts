7d5d39366e16b801dc070c7607db98ec25334081
Lucaszioli <lucas.zioli.04@gmail.com>
2023-10-14 12:43:22 -0300

    fix: conceito errado de stream

--- src/domains/Music/services/serviceMusic.ts
+++ src/domains/Music/services/serviceMusic.ts
@@ -24,16 +24,6 @@ class serviceMusic{
 		if (body.artistaName == '' || !isNaN(Number(body.artistaName))){
 			throw new Error ('O artista precisa de um nome');
 		}
-		const lerArt = await prisma.artist.findUnique({
-			where:{id:body.artistaId}
-		});
-		if (lerArt?.streams != undefined){
-			// eslint-disable-next-line @typescript-eslint/no-unused-vars
-			const updateArt = await prisma.artist.update({
-				where:{id:body.artistaId},
-				data:{streams:lerArt?.streams+1}
-			});
-		}
 		const criar = await prisma.music.create({
 			data:{
 				name: body.name,
@@ -72,33 +62,6 @@ class serviceMusic{
 		if (body.album == ''){
 			throw new Error('O album precisa de um nome');
 		}
-		const ler = await prisma.music.findUnique({
-			where:{id:body.id}
-		});
-		if (ler?.artistaId != null){
-			if (ler.artistaId != body.artistaId){
-				const art1 = await prisma.artist.findUnique({
-					where:{id: ler.artistaId}
-				});
-				const art2 = await prisma.artist.findUnique({
-					where:{id: body.artistaId}
-				});
-				if (art1?.streams != undefined && art2?.streams != undefined){
-					await prisma.artist.update({
-						where:{id:ler.artistaId},
-						data:{
-							streams: art1.streams - 1
-						}
-					});
-					await prisma.artist.update({
-						where:{id:body.artistaId},
-						data:{
-							streams:art2.streams + 1 
-						}
-					});
-				}
-			}
-		}
 		const atualizar = await prisma.music.update({
 			where:{id : body.id},
 			data:{
@@ -117,16 +80,7 @@ class serviceMusic{
 		const deletar = await prisma.music.delete({
 			where:{id: id}
 		});
-		const lerArt = await prisma.artist.findUnique({
-			where:{id: deletar.artistaId}
-		});
-		if (lerArt?.streams != undefined){
-			const atualizarArtista = await prisma.artist.update({
-				where:{ id:deletar.artistaId},
-				data:{streams: lerArt?.streams - 1}
-			});
-			return atualizarArtista;
-		}
+		return deletar;
 	}
 }
 
