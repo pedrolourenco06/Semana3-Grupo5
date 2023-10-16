cbb5066f8648886e29303094a5f1b81b59e4fbf3
Lucaszioli <lucas.zioli.04@gmail.com>
2023-10-16 15:25:24 -0300

    fix: conserta erros de identação

--- src/domains/User/services/UserService.ts
+++ src/domains/User/services/UserService.ts
@@ -1,67 +1,67 @@
-import prisma from "../../../../Client/client";
-import { User } from "@prisma/client";
+import prisma from '../../../../Client/client';
+import { User } from '@prisma/client';
 
 class UserService{
-    async create(body: User){
-        if(body.email == '' || !isNaN(Number(body.email))){
-            throw new Error('O usuário precisa de uma email');
-        }
-        if(body.name == '' || !isNaN(Number(body.name))){
-            throw new Error('O usuário precisa de um nome');
-        }
-        if(body.password == '' || !isNaN(Number(body.password))){
-            throw new Error('O usuário precisa de uma senha');
-        }
+	async create(body: User){
+		if(body.email == '' || !isNaN(Number(body.email))){
+			throw new Error('O usuário precisa de uma email');
+		}
+		if(body.name == '' || !isNaN(Number(body.name))){
+			throw new Error('O usuário precisa de um nome');
+		}
+		if(body.password == '' || !isNaN(Number(body.password))){
+			throw new Error('O usuário precisa de uma senha');
+		}
 
-        const user = await prisma.user.create({
-            data:{
-                email: body.email,
-                name: body.name,
-                password: body.password,
-                photo: body.photo,
-                premium: body.premium,
-            }
-        });
+		const user = await prisma.user.create({
+			data:{
+				email: body.email,
+				name: body.name,
+				password: body.password,
+				photo: body.photo,
+				premium: body.premium,
+			}
+		});
 
-        return user;
-    }
+		return user;
+	}
 
-    async read (body: User){
-        const ler = await prisma.user.findMany();
-        return ler;
-    }
+	async read (){
+		const ler = await prisma.user.findMany();
+		return ler;
+	}
 
-    async update (body: User){
-        if(body.name == ''){
-            throw new Error('O usuário precisa de um nome');
-        }
-        if(body.email == ''){
-            throw new Error('O usuário precisa de um email');
-        }
-        if(body.password == ''){
-            throw new Error('O usuário precisa de uma senha');
-        }
-        const atualizar = await prisma.user.update({
-            where:{email: body.email},
-            data:{
-                name: body.name,
-                password: body.password,
-                photo: body.photo,
-                premium: body.premium
-            }
-        });
+	async update (body: User){
+		if(body.name == ''){
+			throw new Error('O usuário precisa de um nome');
+		}
+		if(body.email == ''){
+			throw new Error('O usuário precisa de um email');
+		}
+		if(body.password == ''){
+			throw new Error('O usuário precisa de uma senha');
+		}
+		const atualizar = await prisma.user.update({
+			where:{email: body.email},
+			data:{
+				name: body.name,
+				password: body.password,
+				photo: body.photo,
+				premium: body.premium
+			}
+		});
 
-        return atualizar;
-    }
+		return atualizar;
+	}
 
-    async delete (email: string){
-        const deletar = await prisma.user.delete({
-            where: {email: email}
-        });
+	async delete (email: string){
+		const deletar = await prisma.user.delete({
+			where: {email: email}
+		});
 
-        return deletar;
+		return deletar;
 
-    }
+	}
 
 }
 
