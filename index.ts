76c1e1cc6b50bbe19bacfdc12c9643a3cf0046b9
Lucaszioli <lucas.zioli.04@gmail.com>
2023-10-16 15:33:40 -0300

    fix: conserta erros de identação

--- index.ts
+++ index.ts
@@ -1,16 +1,16 @@
-import UserService from "./src/domains/User/services/UserService";
+import UserService from './src/domains/User/services/UserService';
 
 async function main(){
-    const body ={
-        email: 'teste2@gmail.com',
-        name: 'teste dois',
-        password: '302111',
-        photo: ' ',
-        teste: ' ',
-        premium: false
-    };
+	const body ={
+		email: 'teste2@gmail.com',
+		name: 'teste dois',
+		password: '302111',
+		photo: ' ',
+		teste: ' ',
+		premium: false
+	};
 
-    const user = await UserService.read(body);
+	const user = await UserService.read();
     
 }
 
--- src/domains/Artist/controller/controllerArtist.ts
+++ src/domains/Artist/controller/controllerArtist.ts
@@ -1,42 +1,42 @@
-import { Artist } from "../model/modelArtist";
-import serviceArtist from "../service/serviceArtist";
+import { Artist } from '../model/modelArtist';
+import serviceArtist from '../service/serviceArtist';
 
 export async function criarArtista(body: Artist) {
-    try{
-        const criar = serviceArtist.create(body);
-        console.log(criar)
-    }
-    catch(error){
-        console.log(error);
-    }
+	try{
+		const criar = serviceArtist.create(body);
+		console.log(criar);
+	}
+	catch(error){
+		console.log(error);
+	}
 }
 
-export async function lerArtista(body: Artist) {
-    try{
-        const ler = serviceArtist.read(body);
-        console.log(ler);
-    }
-    catch(error){
-        console.log(error);
-    }
+export async function lerArtista() {
+	try{
+		const ler = serviceArtist.read();
+		console.log(ler);
+	}
+	catch(error){
+		console.log(error);
+	}
 }
 
 export async function atualizarArtista(body: Artist) {
-    try{
-        const atualizar = serviceArtist.update(body);
-        console.log(atualizar);
-    }
-    catch(error){
-        console.log(error);
-    }
+	try{
+		const atualizar = serviceArtist.update(body);
+		console.log(atualizar);
+	}
+	catch(error){
+		console.log(error);
+	}
 }
 
 export async function deletarArtista(body: Artist) {
-    try{
-        const deletar = serviceArtist.delete(body);
-        console.log(deletar);
-    }
-    catch(error){
-        console.log(error);
-    }
+	try{
+		const deletar = serviceArtist.delete(body);
+		console.log(deletar);
+	}
+	catch(error){
+		console.log(error);
+	}
 }
