a4aed0b07a30509a154608fac58ced88dada072c
Lucaszioli <lucas.zioli.04@gmail.com>
2023-10-28 00:30:31 -0300

    bug-fix(Musicupdate):retira erro de update musica

--- src/domains/User/controller/index.ts
+++ src/domains/User/controller/index.ts
@@ -1,57 +1,57 @@
-import UserService from "../services/UserService";
+import UserService from '../services/UserService';
 import { Router, Request, Response, NextFunction } from 'express';
 
 const router = Router();
 
 router.get('/', async(req: Request, res: Response, next: NextFunction) => {
-    try{
-        const users = await UserService.findUsers();
-        res.json(users);
+	try{
+		const users = await UserService.findUsers();
+		res.json(users);
 
-    }
-    catch(error){
-        next(error);
-    }
+	}
+	catch(error){
+		next(error);
+	}
 });
 
 router.get('/:email', async(req: Request, res: Response, next: NextFunction) => {
-    try{
-        const user = await UserService.findByEmail(req.params.email);
-        res.json(user);
-    }
-    catch(error){
-        next(error);
-    }
+	try{
+		const user = await UserService.findByEmail(req.params.email);
+		res.json(user);
+	}
+	catch(error){
+		next(error);
+	}
 });
 
 router.post('/create', async(req: Request, res: Response, next: NextFunction) => {
-    try{
-        await UserService.create(req.body);
-        res.json('Usuario criado com sucesso!');
-    }
-    catch(error){
-        next(error);
-    }
+	try{
+		await UserService.create(req.body);
+		res.json('Usuario criado com sucesso!');
+	}
+	catch(error){
+		next(error);
+	}
 });
 
 router.delete('/delete/:email', async(req: Request, res: Response, next: NextFunction) => {
-    try{
-        const user = await UserService.delete(req.params.email);
-        res.json(user);
-    }
-    catch(error){
-        next(error);
-    }
+	try{
+		const user = await UserService.delete(req.params.email);
+		res.json(user);
+	}
+	catch(error){
+		next(error);
+	}
 });
 
 router.put('/update', async(req: Request, res: Response, next: NextFunction) => {
-    try{
-        const user = await UserService.update(req.body);
-        res.json(user);
-    }
-    catch(error){
-        next(error);
-    }
+	try{
+		const user = await UserService.update(req.body);
+		res.json(user);
+	}
+	catch(error){
+		next(error);
+	}
 });
 
 
