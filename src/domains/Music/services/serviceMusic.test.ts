2cc67b488205c10a5ac42fd7fb0cb30c9fd9822c
Lucaszioli <lucas.zioli.04@gmail.com>
2023-11-12 21:29:30 -0300

    test: adiciona os testes de erro do updateMusic

--- src/domains/Music/services/serviceMusic.test.ts
+++ src/domains/Music/services/serviceMusic.test.ts
@@ -14,6 +14,7 @@ jest.mock('../../../../config/client.ts',()=>({
 	},
 	music:{
 		create:jest.fn(),
+		update:jest.fn(),
 	}
 }));
 jest.mock('../../Artist/service/serviceArtist.ts',()=>{
@@ -30,7 +31,7 @@ describe('create', () => {
 	});
 
 
-	test('metodo recebe um objeto com informações da musica=> chama o create com os dados corretos', async()=>{
+	test('Metodo recebe um objeto com informações da musica=> chama o create com os dados corretos', async()=>{
 		const mockMusic = {
 			name: 'Teste',
 			genero:'Teste',
@@ -161,3 +162,182 @@ describe('create', () => {
 	});
 
 });
+
+describe('update',()=>{
+	beforeEach(()=>{
+		jest.restoreAllMocks();
+		jest.clearAllMocks();
+	});
+	test('Metodo recebe um objeto com valores de musica => da update no banco de dados',async()=>{
+		const updateMock = {
+			id: 1,
+			name: 'Teste',
+			genero:'Teste',
+			album:'Teste',
+			artistaId:1
+		}as Music;
+		jest.mocked(serviceArtist).findArtist.mockImplementation(
+			()=>{
+				return {} as any;
+			}
+		);
+
+		const musicUpdateMock = jest.spyOn(prisma.music, 'update').mockImplementation(
+			()=>{
+				return {} as any;
+			}
+		);
+
+		await serviceMusic.update(updateMock);
+
+		expect(musicUpdateMock).toBeCalledTimes(1);
+		expect(musicUpdateMock.mock.calls[0][0]).toEqual({
+			data:{
+				name: updateMock.name,
+				genero: updateMock.genero,
+				album:updateMock.album,
+				artistaId:updateMock.artistaId,
+			},
+			where:{
+				id: updateMock.id
+			}
+			
+		});
+	});
+
+	test('Metodo recebe um id como string => Retorna um erro', async()=>{
+		const updateMock = {
+			id: '',
+			name: 'Teste',
+			genero:'Teste',
+			album:'Teste',
+			artistaId:1
+		};
+
+		return expect(
+			async ()=>{
+				await serviceMusic.update(updateMock);
+			}
+		).rejects.toThrowError(new QueryError ('Id da musica precisa ser um número'));
+	});
+
+	test('Metodo recebe um id como 0 => Retorna um erro', async()=>{
+		const updateMock = {
+			id: 0,
+			name: 'Teste',
+			genero:'Teste',
+			album:'Teste',
+			artistaId:1
+		};
+
+		return expect(
+			async ()=>{
+				await serviceMusic.update(updateMock);
+			}
+		).rejects.toThrowError(new QueryError ('Id da musica precisa ser um número'));
+	});
+
+	test('Metodo recebe um artistaId como string => Retorna um erro', async()=>{
+		const updateMock = {
+			id: 1,
+			name: 'Teste',
+			genero:'Teste',
+			album:'Teste',
+			artistaId:''
+		};
+
+		return expect(
+			async ()=>{
+				await serviceMusic.update(updateMock);
+			}
+		).rejects.toThrowError(new QueryError ('Id do artista precisa ser um número'));
+	});
+
+	test('Metodo recebe um artistaId como 0 => Retorna um erro', async()=>{
+		const updateMock = {
+			id: 1,
+			name: 'Teste',
+			genero:'Teste',
+			album:'Teste',
+			artistaId:0
+		};
+
+		return expect(
+			async ()=>{
+				await serviceMusic.update(updateMock);
+			}
+		).rejects.toThrowError(new QueryError ('Id do artista precisa ser um número'));
+	});
+
+	test('Metodo recebe um nome vazio => Retorna um erro', async()=>{
+		const updateMock = {
+			id: 1,
+			name: '',
+			genero:'Teste',
+			album:'Teste',
+			artistaId:1
+		};
+
+		return expect(
+			async()=>{
+				await serviceMusic.update(updateMock);
+			}
+		).rejects.toThrowError(new QueryError('A musica precisa de um nome'));
+	});
+
+	test('Metodo recebe um genero vazio => Retorna um erro', async()=>{
+		const updateMock = {
+			id: 1,
+			name: 'Teste',
+			genero: '',
+			album:'Teste',
+			artistaId:1
+		};
+
+		return expect(
+			async()=>{
+				await serviceMusic.update(updateMock);
+			}
+		).rejects.toThrowError(new QueryError('O genero precisa de um nome'));
+	});
+	
+	test('Metodo recebe um album vazio => Retorna um erro', async()=>{
+		const updateMock = {
+			id: 1,
+			name: 'Teste',
+			genero:'Teste',
+			album:'',
+			artistaId:1
+		};
+
+		return expect(
+			async()=>{
+				await serviceMusic.update(updateMock);
+			}
+		).rejects.toThrowError(new QueryError('O album precisa de um nome'))
+	});
+
+	test('Metodo recebe um artista inexistente => Retorna um erro', async()=>{
+		const updateMock = {
+			id: 1,
+			name: 'Teste',
+			genero:'Teste',
+			album:'Teste',
+			artistaId:1
+		};
+
+		jest.mocked(serviceArtist).findArtist.mockImplementation(
+			()=>{
+				return undefined as any;
+			}
+		);
+
+		return expect(
+			async()=>{
+				await serviceMusic.update(updateMock);
+			}
+		).rejects.toThrowError(new QueryError('O artista tem que existir'));
+	});
+});
+
+
